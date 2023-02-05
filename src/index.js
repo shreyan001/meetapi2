14989d0fcf1361c36c96d22f56e5f3adc445fc7e
shreyan001 <shreyan010@gmail.com>
2023-02-05 21:32:15 +0530

    change2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 const port = process.env.PORT || 3200;
 app.listen(port, () => {
+  app.use(express.static('public'));
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
